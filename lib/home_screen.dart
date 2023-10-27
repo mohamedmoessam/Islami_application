9356837ca6e444f4e51da58f97b0f7df5ac8bb4d
mohamedmoessam <mohmed2002mohamed@gmail.com>
2023-10-27 18:55:00 +0200

    initial project

---
+++ lib/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String RouteName='home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
